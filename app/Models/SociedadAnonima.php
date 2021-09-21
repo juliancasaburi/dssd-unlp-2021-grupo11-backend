50dde87dde7fda0fd412ef976dd9edc5d189dde5
Julian Casaburi <juliancasaburi@gmail.com>
2021-09-21 02:36:11 -0300

    Se agregan modelos y migraciones de S.A. y Socio

---
+++ app/Models/SociedadAnonima.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SociedadAnonima extends Model
+{
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'sociedades_anonimas';
+
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre',
+        'fecha_creacion',
+        'domicilio_legal',
+        'domicilio_real',
+        'id_apoderado',
+        'email_apoderado',
+        'numero_expediente',
+        'numero_hash',
+        'url_codigo_QR',
+        'estado_evaluacion',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'fecha_creacion' => 'datetime',
+    ];
+
+    /**
+     * Obtener los socios.
+     */
+    public function socios()
+    {
+        return $this->hasMany(Socio::class);
+    }
+
+    /**
+     * Obtener el apoderado.
+     */
+    public function apoderado()
+    {
+        return Socio::find($this->idApoderado);
+    }
+}
---
+++ database/migrations/2021_09_21_050609_create_socios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSociosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socios', function (Blueprint $table) {
+            $table->id();
+            $table->string('apellido');
+            $table->string('nombre');
+            $table->integer('porcentaje');
+            $table->timestamps();
+
+            $table->foreign('id_sociedad')
+                ->references('id')
+                ->on('sociedades_anonimas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socios');
+    }
+}
