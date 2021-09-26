1c48dbe195db35f6710e85093e635d0de58a78fa
Julian Casaburi <juliancasaburi@gmail.com>
2021-09-26 19:46:43 -0300

    Fix validation errors responses

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -110,7 +110,7 @@ class AuthController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
+            return response()->json($validator->errors(), 400);
         }
 
         try {
