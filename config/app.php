d8fd6fbc75ba74af86f568b7adebdffcfca2ce16
Julian Casaburi <juliancasaburi@gmail.com>
2021-09-28 15:36:44 -0300

    Se agrega package y config flysystem-google-drive

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Google Drive...
+         */
+        App\Providers\GoogleDriveServiceProvider::class,
+
     ],
 
     /*
