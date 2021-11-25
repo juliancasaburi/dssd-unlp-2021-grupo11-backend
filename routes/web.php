59cd71cad2be728241ecc7fde2f6cf7dc82892e2
Julian Casaburi <juliancasaburi@gmail.com>
2021-11-25 18:18:29 -0300

    🔥Remove emailTest route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Mail\WelcomeMailable;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Web\SociedadAnonimaController;
 
@@ -19,12 +17,6 @@ use App\Http\Controllers\Web\SociedadAnonimaController;
 
 Auth::routes(['verify' => true]);
 Route::get('/', [App\Http\Controllers\Web\HomeController::class, 'index'])->name('web.index');
-
-/*Email example */
-Route::get('/emailTest', function(){
-    $email = new WelcomeMailable;
-    Mail::to('someEmailAddress@domain')->send($email);
-});
 
 /* Info pública S.A */
 Route::get('/sa/{numeroHash}', [SociedadAnonimaController::class, 'infoPublicaSA']);
